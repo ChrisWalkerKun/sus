425ff7125b95209b524604e3e00cdfe8f7ef49ab
chriswalkerkun <chienvmgcs210828@fpt.edu.vn>
2023-05-29 16:20:35 +0700

    update app_mid.js

--- app_mid.js
+++ app_mid.js
@@ -2,11 +2,10 @@ const express = require ( 'express' );
 const app = express();
 
 app.use((req, res, next) => {
-	console.log( 'Time:' , Date.now());
-	next();
-
-    let date = new Date().toLocaleString();
+    let date = new Date(Date.now()).toLocaleString();
     res.send(date);
+    console.log( 'Time:' , date);
+	next();
 });
 
 app.listen(process.env.port || 3000 );
